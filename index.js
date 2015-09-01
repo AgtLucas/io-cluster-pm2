e720361865a0444eade3896a98c8ef3a3517d282
Lucas <lucas@agtlucas.com>
2015-09-01 08:24:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello!');
 }).listen(8080);
+
+process.on('message', function(msg) {
+  if (msg === 'shutdown') {
+    close_all_connections();
+    delete_cache();
+    server.close();
+    process.exit(0);
+  }
+});
