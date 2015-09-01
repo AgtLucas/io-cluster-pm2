a67e0af916520d1f40253de377b2ceb26e5507ef
Lucas <lucas@agtlucas.com>
2015-08-31 23:15:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 'use strict';
 
-var cluster = require('cluster');
 var http = require('http');
-var os = require('os');
 
-var numCPUs = os.cpus().length;
-
-if (cluster.isMaster) {
-  for (var i = 0; i <numCPUs; ++i) {
-    cluster.fork();
-  }
-} else {
-  http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Hello!');
-  }).listen(8080);
-}
+http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello!');
+}).listen(8080);
