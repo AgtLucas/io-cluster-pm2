0f0d6115c88ec6f98091db4b03b6a485aca9d878
Lucas <AgtLucas@users.noreply.github.com>
2016-01-11 09:11:04 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-var http = require('http');
+var http = require('http')
 
 http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello!');
-}).listen(8080);
+  res.writeHead(200)
+  res.end('Hello!')
+}).listen(8080)
 
 process.on('message', function(msg) {
   if (msg === 'shutdown') {
-    close_all_connections();
-    delete_cache();
-    server.close();
-    process.exit(0);
+    close_all_connections()
+    delete_cache()
+    server.close()
+    process.exit(0)
   }
-});
+})
